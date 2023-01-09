832347d742f68ac81241544deb0605adf75729d6
Yubraj <nontoxicyubs@gmail.com>
2023-01-09 20:18:15 +0545

    Updates

---
+++ precache-manifest.8c24b4c141647ce45937e929a5d50f54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7061644f26a039b13b62",
+    "url": "/e-commerce/static/css/main.bbfacbd6.chunk.css"
+  },
+  {
+    "revision": "7061644f26a039b13b62",
+    "url": "/e-commerce/static/js/main.7061644f.chunk.js"
+  },
+  {
+    "revision": "6f6f4986655de22842af",
+    "url": "/e-commerce/static/js/1.6f6f4986.chunk.js"
+  },
+  {
+    "revision": "2a17a5a387125ab8e714",
+    "url": "/e-commerce/static/js/2.2a17a5a3.chunk.js"
+  },
+  {
+    "revision": "f33fbc48922226cc39e2",
+    "url": "/e-commerce/static/js/runtime~main.f33fbc48.js"
+  },
+  {
+    "revision": "6fb359dbf87b6f6607e9d4a37d2fb448",
+    "url": "/e-commerce/index.html"
+  }
+];
