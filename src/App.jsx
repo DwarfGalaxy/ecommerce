93baef607e68006aff546832706a37010bb5919b
Yubraj <nontoxicyubs@gmail.com>
2023-01-09 20:32:10 +0545

    list and grid view has been changed

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <BrowserRouter>
       <Header/>
         <Routes>
-          <Route exact path='/' element={<Home />} />
+          <Route path='/' element={<Home />} />
           <Route exact path='/about' element={<About />} />
           <Route exact path='/products' element={<Products />}/>
           <Route exact path='/contact' element={<Contact />} />
