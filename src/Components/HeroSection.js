9317aa3299c4403354c0608be5f5c5c6d0778e9d
Yubraj <nontoxicyubs@gmail.com>
2023-03-08 23:28:04 +0545

    home page has been modified

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function HeroSection(props) {
+    const navigate = useNavigate();
+
     return (
         <>
             <div className="container-fluid">
                 <div className="container py-5">
-                <div className="row">
-                    <div className="col-sm-12 col-xs-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6   my-auto">
-                        <p className='text-secondary mb-0  '>Welcome to</p>
-                        <p className='fs-2 fw-bolder  mb-0'>E-Commerce {props.title}</p>
-                       <p className='text-secondary'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, esse! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, tempora.</p> 
-                       <button type="button" className="btn btn-success">SHOW NOW</button>
+                    <div className="row">
+                        <div className="col-sm-12 col-xs-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6   my-auto">
+                            <p className='text-secondary mb-0  '>Welcome to</p>
+                            <p className='fs-2 fw-bolder  mb-0'>E-Commerce {props.title}</p>
+                            <p className='text-secondary'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, esse! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur, tempora.</p>
+                            <button type="button" className="btn btn-success" onClick={() => navigate('/products')}>SHOW NOW</button>
+                        </div>
+                        <div className="col mt-2">
+                            <img src="https://i2.wp.com/eatatefood.com/wp-content/uploads/2018/04/shutterstock_481902994.jpg?resize=2048%2C1367&ssl=1" alt="" id='main-img' className='img-fluid rounded ' />
+                        </div>
                     </div>
-                    <div className="col mt-2">
-                        <img src="https://i2.wp.com/eatatefood.com/wp-content/uploads/2018/04/shutterstock_481902994.jpg?resize=2048%2C1367&ssl=1" alt="" id='main-img' className='img-fluid rounded ' />
-                    </div>
-                </div>
                 </div>
             </div>
         </>
