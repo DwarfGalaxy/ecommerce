77e0f1319e74f1c730441d859a3bf3a0f1d6d13e
Yubraj <nontoxicyubs@gmail.com>
2023-03-08 22:14:11 +0545

    main page has been changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import About from './Components/About';
-import Cart from './Components/Cart';
-import Contact from './Components/Contact';
-import ErrorPage from './Components/ErrorPage';
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import Home from './Components/Home';
-import Products from './Components/Products';
-import SingleProduct from './Components/SingleProduct';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import About from "./Components/About";
+import Cart from "./Components/Cart";
+import Contact from "./Components/Contact";
+import ErrorPage from "./Components/ErrorPage";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Products from "./Components/Products";
+import SingleProduct from "./Components/SingleProduct";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Header/>
+        <Header />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route exact path='/about' element={<About />} />
-          <Route exact path='/products' element={<Products />}/>
-          <Route exact path='/contact' element={<Contact />} />
-          <Route exact path='/cart' element={<Cart />} />
-          <Route exact path='/:id' element={<SingleProduct />} />
-          <Route path='*' element={<ErrorPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/ecommerce" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/products" element={<Products />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route exact path="/:id" element={<SingleProduct />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </>
   );
